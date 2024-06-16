6594fa26f1d97319890756baefde6aa411e2813b
Marek Smieja <67819687+marek-onPC@users.noreply.github.com>
2024-06-16 21:04:58 +0900

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -38,3 +38,23 @@ def test_main_tabs(mock_app):
 
     # Assert exit button
     assert button.cget("text") == "Exit"
+
+
+def test_main_tabs(mock_app):
+    app, frame = mock_app
+
+    tab = frame.winfo_children()[0]
+    button = frame.winfo_children()[1]
+
+    tab = cast(customtkinter.CTkTabview, tab)
+    button = cast(customtkinter.CTkButton, button)
+
+    # Assert current view
+    assert tab.get() == "Backup"
+
+    # Select and assert 'Restore' view
+    tab.set("Restore")
+    assert tab.get() == "Restore"
+
+    # Assert exit button
+    assert button.cget("text") == "Exit"
